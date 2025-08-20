62767a4b1b12de7f8a5ff86c15dd5909edba92ad
Mir Kashif <138784444+mirkashi@users.noreply.github.com>
2025-08-20 08:18:35 +0000

    data types

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";//treat all js code as new version
+//alert(3+3) //we are using nodejs ,not browser.
+
+console.log(8+
+    9)//code readability should be high
+
+console.log("messi")
+
+let name=("kashi")
+let age= 23
+let loggedIn = false
+let state;
+
+//number => 2 to power 59 or 68
+//bigint =>
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+
+//object
+console.log(typeof undefined)
