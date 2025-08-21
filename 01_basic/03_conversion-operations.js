cbe900c9c78fc055d0680cd69e6c14dfc76704f6
Loop Loard <138784444+mirkashi@users.noreply.github.com>
2025-08-21 18:21:12 +0000

    conversion of data types

---
+++ 01_basic/03_conversion-operations.js
@@ -0,0 +1,32 @@
+let score = "true"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+ //"33" => easy converted in 33.
+ //"33abc" => NaN.
+ //"true" => 1; false=> 0;
+
+
+//let isLoggedIn = "Loop Loard"
+
+//let BooleanisLoggedIn = Boolean(isLoggedIn)
+
+//console.log(BooleanisLoggedIn);
+
+
+//1  => true, 0 => false,
+//"" => false,
+//"loop loard" => true
+
+let somenumber = 34
+
+let stringnumber = String(somenumber);
+console.log(stringnumber);
+console.log(typeof stringnumber);
