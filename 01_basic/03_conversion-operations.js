0b6d74b4d4fb5cc9bb92010e6cc1ab61e2d380f0
Loop Loard <138784444+mirkashi@users.noreply.github.com>
2025-08-22 15:23:10 +0000

    conversion operation studies

--- 01_basic/03_conversion-operations.js
+++ 01_basic/03_conversion-operations.js
@@ -34,3 +34,45 @@ let stringnumber = String(somenumber);
 /***************Operations********************* */
 
 let value = 43
+let negvalue = -value
+
+//console.log(negvalue);
+
+
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2**2)
+//console.log(2/4)
+//console.log(2%5)
+
+let str1= "Lionel"
+let str2=" Messi"
+
+let str3=str1+str2
+
+//console.log(str3)
+
+
+//console.log("1"+2);
+//console.log(2+"3");
+//console.log("2"+ 3 + 4);
+//console.log(2+ 3 + "4");
+
+//console.log((2 + 4) * 6 / 9);
+
+//console.log(+true)
+//console.log(+"")
+
+let num1,num2,num3
+
+num1=num2=num3=3+3
+
+let Gamecounter=100
+
+++Gamecounter;
+
+console.log(Gamecounter);
+
+//link to study conversion operation .
+//https://262.ecma-international.org/16.0/index.html?_gl=1*qlbobm*_ga*MTk4OTA0MjgwNy4xNzU1NjczODkz*_ga_TDCK4DWEPP*czE3NTU4NzYwOTQkbzQkZzAkdDE3NTU4NzYwOTQkajYwJGwwJGgw#sec-type-conversion
