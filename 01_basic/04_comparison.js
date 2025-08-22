46a3ab93cd8282c3eee2ebe18f1697c8b008b94d
Loop Loard <138784444+mirkashi@users.noreply.github.com>
2025-08-22 16:19:59 +0000

    comparison data types

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,22 @@
+//console.log(2 > 1 );
+//console.log(2 >=1);
+//console.log(2 < 1);
+//console.log(2 ==2);
+//console.log(2 !=1);
+
+
+//console.log("2">1);
+//console.log("2"> 1);
+
+console.log(null >0);//********line 11 to line no 17 avoid these type of conversion comparison***/
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+/******strick check****** */
+// checking with ===
+
+console.log("2" === 2);
