2fe5361433fac145a004b9cae09181ea28134f08
Loop Lord <138784444+mirkashi@users.noreply.github.com>
2025-08-23 21:25:27 +0000

    datatype-summary

---
+++ 01_basic/05_Datatypes-summary.js
@@ -0,0 +1,44 @@
+//primitive
+
+//7 types : string , call by value.
+//number, boolearn, null, nudefined, symbol , BigInt
+
+//this is primitive data types.
+
+const score = 1000
+
+const scoreValue = 100.2
+
+
+const isLoggedIn = false
+const outsidetemp= null
+let userEmail;
+
+const id = Symbol ('123')
+
+const anotherid =Symbol('123')
+
+console.log(id === anotherid);
+
+
+//const bignum = 249484899494994949949n
+
+
+//reference(non primitive)
+
+//array, objects, functions
+
+const heros = ["Messi","ronaldo","neymar"]
+let myobj=
+{
+    name:"kashi",
+    age:23
+}
+
+const myfunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof anotherid);
+
+//http://262.ecma-international.org/5.1/#sec-11.4.3
