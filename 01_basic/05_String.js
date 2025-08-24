ab9f7abc7b2b6edff9578c62219c7955c3ff1682
Loop Lord <138784444+mirkashi@users.noreply.github.com>
2025-08-24 18:38:27 +0000

    string discuss

--- 01_basic/05_String.js
+++ 01_basic/05_String.js
@@ -1,7 +1,12 @@
-const name = " Kashi"
+const name = "Kashi"
 
 const age = 23
 
 // console.log(name +  age + " Killer");
 
-console.log('hello my name is kashif and my age is 23' );
+console.log(`hello my name is ${name} and my age is ${age}`);
+
+const gamename = new String(`PUBG`)
+
+console.log(gamename[1]);
+console.log(gamename.__proto__);
