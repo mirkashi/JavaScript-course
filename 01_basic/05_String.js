e054e916def1a83ca5ba4c22c9cb4ac31af8f966
Loop Lord <138784444+mirkashi@users.noreply.github.com>
2025-08-25 16:14:44 +0000

    string value

--- 01_basic/05_String.js
+++ 01_basic/05_String.js
@@ -4,14 +4,23 @@ const age = 23
 
 // console.log(name +  age + " Killer");
 
-console.log(`hello my name is ${name} and my age is ${age}`);
+//console.log(`hello my name is ${name} and my age is ${age}`);
 
-const gamename = new String(`PUBG`)
+const gamename = new String(`PUBG-GAME`)
 
 console.log(gamename[0]);
 console.log(gamename.__proto__);
 
 
-console.log(gamename.length);
-console.log(gamename.toUpperCase);
+//console.log(gamename.length);
+//console.log(gamename.toUpperCase);
 console.log(gamename.charAt(3));
+console.log(gamename.indexOf('G'));
+
+const newstring = gamename.substring(0,4)
+
+console.log(newstring);
+
+const anotherstring = gamename.slice(-8, 4)
+
+console.log(anotherstring);
