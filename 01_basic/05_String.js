3bf6d3d8afcb68e0b2a0b4eb1b4eb7c66c120111
Loop Lord <138784444+mirkashi@users.noreply.github.com>
2025-08-25 16:06:07 +0000

    charachet position

--- 01_basic/05_String.js
+++ 01_basic/05_String.js
@@ -8,5 +8,10 @@ console.log(`hello my name is ${name} and my age is ${age}`);
 
 const gamename = new String(`PUBG`)
 
-console.log(gamename[1]);
+console.log(gamename[0]);
 console.log(gamename.__proto__);
+
+
+console.log(gamename.length);
+console.log(gamename.toUpperCase);
+console.log(gamename.charAt(3));
