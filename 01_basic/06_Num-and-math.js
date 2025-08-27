53add3e735ea976897e0bb82b7b8cb203ba01499
Loop Lord <138784444+mirkashi@users.noreply.github.com>
2025-08-27 09:28:08 +0000

    number and math pratice

--- 01_basic/06_Num-and-math.js
+++ 01_basic/06_Num-and-math.js
@@ -1,6 +1,31 @@
 const score = 400
-console.log(score);
+//console.log(score);
 
-const balance = new Number(100)
-console.log(balance.toString().length);
-console.log(balance.toFixed(1));
+const balance = new Number(100.)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(1));
+
+const othernumber = 223.987
+
+//console.log(othernumber.toPrecision(3))
+
+const hunderds = 100000
+
+//console.log(hunderds.toLocaleString('en-in'));
+
+//++++++++++++++++++++ Maths ++++++++++++++++
+
+console.log(Math);
+
+//console.log(Math.abs(-4));
+//console.log(Math.round(4.8));
+//console.log(Math.ceil(4.2));
+//console.log(Math.floor(5.9));
+//console.log(Math.min(4, 3, 6, 8));
+//console.log(Math.max(4, 5, 8, 9));
+
+console.log(Math.random());
+console.log(Math.random()*10 + 1);
+console.log(Math.random()*10 + 1);
