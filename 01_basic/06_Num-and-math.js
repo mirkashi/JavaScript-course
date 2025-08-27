eafd488e35084ebeb6a106e8d0237e98b0e3a631
Loop Lord <138784444+mirkashi@users.noreply.github.com>
2025-08-27 09:39:31 +0000

    final done number and math

--- 01_basic/06_Num-and-math.js
+++ 01_basic/06_Num-and-math.js
@@ -28,4 +28,9 @@ console.log(Math);
 
 console.log(Math.random());
 console.log(Math.random()*10 + 1);
-console.log(Math.random()*10 + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 20;
+const max = 30;
+
+console.log(Math.floor(Math.random() * (max - min + 1) ) + min)
