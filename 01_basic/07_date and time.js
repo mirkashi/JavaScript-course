db22fa0c18bbe26db00f303c387eab8049432dd5
Loop Lord <138784444+mirkashi@users.noreply.github.com>
2025-08-29 15:31:18 +0000

    date and time reading

---
+++ 01_basic/07_date and time.js
@@ -0,0 +1,10 @@
+//Dates
+
+let mydate = new Date()
+//console.log(mydate.toString());
+//console.log(mydate.toDateString());
+//console.log(mydate.toLocaleString());
+//console.log(typeof mydate);
+
+let mycreatedDate = new Date(2025, 1 ,23)
+console.log(mycreatedDate.toDateString());
