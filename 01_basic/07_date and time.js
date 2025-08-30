556a35ca47b63c39742f9665b3ef46dfe19e4cba
Loop Lord <138784444+mirkashi@users.noreply.github.com>
2025-08-30 11:27:02 +0000

    date and time work

--- 01_basic/07_date and time.js
+++ 01_basic/07_date and time.js
@@ -6,5 +6,12 @@ let mydate = new Date()
 //console.log(mydate.toLocaleString());
 //console.log(typeof mydate);
 
-let mycreatedDate = new Date(2025, 1 ,23)
-console.log(mycreatedDate.toDateString());
+//let mycreatedDate = new Date(2025, 0 ,23)
+//let mycreatedDate = new Date(2025, 0 ,23 ,5 ,6)
+//let mycreatedDate =new Date("2025-01-15")
+let mycreatedDate = new Date ("01-16-2025")
+//console.log(mycreatedDate.toLocaleString());
+
+let mytimestamp = Date.now()
+
+console.log(mytimestamp);
