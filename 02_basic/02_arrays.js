a48a16e7cd52b4223730242906fee6d1bdce5bd3
Loop Lord <138784444+mirkashi@users.noreply.github.com>
2025-09-02 13:46:46 +0000

    array types and their function

---
+++ 02_basic/02_arrays.js
@@ -0,0 +1,10 @@
+const marvel_heros = ["Hulk","thor","aquaman"]
+const dc_heros  = ["superman","batman","spiderman"]
+
+//marvel_heros.push(dc_heros)
+
+//console.log(marvel_heros);
+//console.log(marvel_heros[3][2]);
+
+const allhero = marvel_heros.concat(dc_heros) //using concat tho merg t array in one.
+console.log(allhero);
