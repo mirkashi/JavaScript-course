ebe6408df4e106ece783bd73e7976d269337d902
Loop Lord <138784444+mirkashi@users.noreply.github.com>
2025-09-02 17:36:04 +0000

    object first part complete

--- 02_basic/03_objects.js
+++ 02_basic/03_objects.js
@@ -27,3 +27,25 @@ console.log(Jsuser["full name"]) //jab uper value variable or data string mai ho
 
  //symbol declare
  console.log(Jsuser[Losym]);
+
+
+//object ki value ko access kasy karty hai,dot notaion sy**must b remember this thing
+
+//change value like
+Jsuser.email = "mirkashi111@hotmail.com"
+//value lock kar ny jays k ap chaaty hai k koi b change na kary tu ous k liye hum use karty hia.freez yanee object ko freez kar dyty hai.jasy
+Object.freeze(Jsuser)
+Jsuser.email = "mirkashi12@gmail.com"
+console.log(Jsuser);
+
+//*******ab seekh na hai function kasy add karty hai  */
+Jsuser.greeting = function(){
+    console.log("hello javascript");
+}
+Jsuser.greeting2 = function(){
+    console.log(`hello messi, ${this.locaton}`);
+}
+console.log(Jsuser.greeting());
+console.log(Jsuser.greeting2());
+
+//note jab b value ko access kar na ho tu dot sy kar na hai.
