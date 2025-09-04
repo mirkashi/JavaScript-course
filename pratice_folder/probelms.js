8587ecdee76e3544fc3c6dbc78b168f36f97986f
Loop Lord <138784444+mirkashi@users.noreply.github.com>
2025-09-04 08:35:47 +0000

    problem solve

---
+++ pratice_folder/probelms.js
@@ -0,0 +1,16 @@
+//Write a program that solves the following expression: 
+//      AB
+//-------------D
+//     30C%100
+
+
+let  a=25;
+let b=20;
+let c=35;
+let d=75;
+
+ 
+let result = (a * b * d) /(30 * c %100) ;
+
+console.log(result);
+ 
