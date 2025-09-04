7050827c5056b12bd6d65edd180fc6657d7a85f0
Loop Lord <138784444+mirkashi@users.noreply.github.com>
2025-09-04 16:58:39 +0000

    change

--- pratice_folder/date_math_paratice.js
+++ pratice_folder/date_math_paratice.js
@@ -90,3 +90,21 @@ const dayName = today.toLocaleString("en-US", { weekday: "long" });
 console.log("Today is:", dayName);
 
 
+//date implement function
+
+// new Date()
+// new Date(date string)
+
+// new Date(year,month)
+// new Date(year,month,day)
+// new Date(year,month,day,hours)
+// new Date(year,month,day,hours,minutes)
+// new Date(year,month,day,hours,minutes,seconds)
+// new Date(year,month,day,hours,minutes,seconds,ms)
+
+//  new Date(milliseconds)
+
+// try and pratice every function.
+
+const date=new Date();
+console.log(date);
