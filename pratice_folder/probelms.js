3b2b953e6f2e057dc71ce6217d7a742754eae5ef
Loop Lord <138784444+mirkashi@users.noreply.github.com>
2025-09-04 17:34:05 +0000

    make perfect it again

--- pratice_folder/probelms.js
+++ pratice_folder/probelms.js
@@ -12,5 +12,24 @@ let d=75;
  
 let result = (a * b * d) /(30 * c %100) ;
 
-console.log(result);
+//console.log(result);
  
+// convert current date and time 
+// like this 
+// Example output: Current Date: 3 September 2025, 6:45 PM
+
+//start
+
+let asim = new Date()
+
+let messi={
+month:"long",
+year:"numeric",
+day:"numeric",
+hour:"numeric",
+minute:"numeric",
+hour12:true
+}
+
+let mydate = asim.toLocaleString("en-GB",messi)
+console.log(mydate);
