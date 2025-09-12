dbe3db9fbc9b4808d24485a88a8810405ad2793e
Loop Lord <mirkashi111@gmail.com>
2025-09-12 12:09:13 +0000

    object access in object

--- 02_basic/04_objects.js
+++ 02_basic/04_objects.js
@@ -5,4 +5,18 @@ whatsappuser.id = "kashi"
 whatsappuser.name = "mir"
 whatsappuser.isloggedin =  false
 
-console.log(whatsappuser);
+//console.log(whatsappuser);
+
+const regularuser ={
+
+    email: "mirkashi111@gmail.com",
+    fullname : {
+        usernamess : {
+            fathername : "khalid",
+            userlastname:"mehmood"
+
+        }
+    }
+}
+
+console.log(regularuser.fullname.usernamess.userlastname);
