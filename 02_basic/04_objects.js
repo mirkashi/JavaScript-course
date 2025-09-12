fc2181e7ccc22c6dccf709d30422308bc27b0c87
Loop Lord <mirkashi111@gmail.com>
2025-09-12 11:56:00 +0000

    basci value

---
+++ 02_basic/04_objects.js
@@ -0,0 +1,8 @@
+//const whatsappuser = new Object() /***yai eik singelton object or nechy wala non-singelton like dono mai koi difference nahi.dono he sam hai  */
+const whatsappuser ={}
+
+whatsappuser.id = "kashi"
+whatsappuser.name = "mir"
+whatsappuser.isloggedin =  false
+
+console.log(whatsappuser);
