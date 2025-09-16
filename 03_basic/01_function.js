1dcd6c583e0662f6db19f0c006f4d1f7f3fc667b
Loop Lord <mirkashi111@gmail.com>
2025-09-16 18:57:03 +0000

    array and object in functions

--- 03_basic/01_function.js
+++ 03_basic/01_function.js
@@ -52,4 +52,18 @@ lastname: "mir",
 function course(anyman){
    console.log(`username is ${anyman.username} and lastname is ${anyman.lastname}`);
 }
-course(user)
+//course(user)
+
+course({
+   username:"kashi",
+   lastname:"mir"
+})
+
+const mynewarray = [200,400,500,600]
+
+function returnVALUE(getArray){
+   return getArray[1]
+
+}
+// console.log(returnVALUE(mynewarray));
+console.log(returnVALUE([200,400,500,600]));
