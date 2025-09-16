c92df814955b0aefd88bdc6e2e7dc249e2b40aad
Loop Lord <mirkashi111@gmail.com>
2025-09-16 16:35:42 +0000

    functions output

---
+++ 03_basic/01_function.js
@@ -0,0 +1,20 @@
+
+function meranaam(){
+
+console.log("K");
+console.log("a");
+console.log("s");
+console.log("h");
+console.log("i");
+console.log("f");
+
+}
+
+//meranaam()
+
+function addtwonumber(num1,num2){
+
+   console.log(num1 + num2)
+}
+
+addtwonumber(4,null)
